4179da6744168c495cd81b90d4c60455dd59eae2
Antoni Duda <antoniduda03@protonmail.com>
2024-06-22 18:25:39 +0200

    Converting output to be csv compatible

--- main.py
+++ main.py
@@ -319,27 +319,18 @@ def check_gate(gate, sumator, a, b, state):
     res = int(''.join([str(int(a)) for a in sumator.get_state()]), 2)
     s = a + b
     parent = gate
-    print(f"While performing addition of {a} and {b}:")
+    row = [a, b, state, s, res]
+    path = []
     while True:
-        print(parent, end=" -> ")
+        path.append(str(parent))
         if parent.parent is None:
             break
         parent = parent.parent
-    print(f"\b\b\bwas forced to {state} leading to:")
-    print(f"Excepted result {s}, actual result {res}")
-    correct = False
-    detected = False
-    if res == s:
-        correct = True
-        print("Correct result")
-    else:
-        print("Incorrect result")
-    if comparator.get_state():
-        detected = True
-        print("Detected by comparator circuit")
-    else:
-        print("Not detected by comparator circuit")
-    print("")
+    row +=[ " -> ".join(path)]
+    correct = (res == s)
+    detected = comparator.get_state()
+    row += [correct, detected]
+    print("|".join(map(str, row)))
     gate.force_state = None
     return detected, correct
 
@@ -347,6 +338,7 @@ if __name__ == "__main__":
     total_tests = 0
     total_correct = 0
     total_detected = 0
+    print("A|B|wymuszony stan|Oczekiwany wynik|Otrzymany wynik|bramka|Czy poprawny wynik|Czy wykryty")
     for a in range(0, 2**4):
         for b in range(0, 2**4):
             a_io = get_4bit_io_from_num(a)
@@ -370,5 +362,4 @@ if __name__ == "__main__":
                 total_detected += d
                 total_tests += 2
             Gate.gates.clear()
-    print(f"\n\nTotal tests: {total_tests}, detected errors: {total_detected}, correct results: {total_correct}")
 
